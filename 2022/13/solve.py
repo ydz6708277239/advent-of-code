20b58e9d211bfa5c13ce075cf0b5364acc792a81
wuda20 <daniel.wu35@qmail.cuny.edu>
2022-12-06 00:29:19 -0500

    finished Day 6: Tuning Trouble

--- 2022/13/solve.py
+++ 2022/13/solve.py
@@ -15,46 +15,33 @@ dir_path = path[0]  ## get cwd
 mov = [-1,0,1]
 mv = [-1,0,1,0,-1]
 
-adj8 = lambda i,j: [(i+di,j+dj) for di,dj in [*(product(mov,mov))] if di|dj]
+# adj8 = lambda i,j: [(i+di,j+dj) for di,dj in [*(product(mov,mov))] if di|dj]
 # adj4 = [(i,j) for i,j in adj8 if i^j]
-adj4 = lambda i,j: [(i+di,j+dj) for di,dj in zip(mv[:-1], mv[1:])]
+# adj4 = lambda i,j: [(i+di,j+dj) for di,dj in zip(mv[:-1], mv[1:])]
+def adj8(i,j):
+    yield from ((i+di,j+dj) for di,dj in [*(product(mov,mov))] if di|dj)
+def adj4(i,j):
+    yield from ((i+di,j+dj) for di,dj in zip(mv[:-1], mv[1:]))
 
 """
 
 """
-test = 1
+test = 0
 fi = "input.txt" if not test else "ex1.txt"
 fip = join(dir_path, fi)
 
 padding = 0
 pad = ['\n'] if padding else []
 
-ans = 0
+a, b = 0, 0
 with open(fip) as f:
     for idx, line in enumerate(f.readlines() + pad):
         xs = line.strip()
+        print(xs)
 
 
 
-    print(f'p1 ans: {ans}')
+    print(f'p1 ans: {a}')
 
-"""
-
-"""
-test = 1
-fi = "input.txt" if not test else "ex1.txt"
-fip = join(dir_path, fi)
-
-padding = 0
-pad = ['\n'] if padding else []
-
-ans = 0
-with open(fip) as f:
-    for idx, line in enumerate(f.readlines() + pad):
-        xs = line.strip()
-
-
-
-    print(f'p2 ans: {ans}')
-
+    print(f'p2 ans: {b}')
 
--- 2022/6/solve.py
+++ 2022/6/solve.py
@@ -20,9 +20,12 @@ adj8 = lambda i,j: [(i+di,j+dj) for di,dj in [*(product(mov,mov))] if di|dj]
 adj4 = lambda i,j: [(i+di,j+dj) for di,dj in zip(mv[:-1], mv[1:])]
 
 """
-
+The device will send your subroutine a datastream buffer (your puzzle input); your subroutine needs to identify 
+the first position where the four most recently received characters were all different. Specifically, it needs 
+to report the number of characters from the beginning of the buffer to the end of the first such four-character marker.
+How many characters need to be processed before the first start-of-packet marker is detected?
 """
-test = 1
+test = 0
 fi = "input.txt" if not test else "ex1.txt"
 fip = join(dir_path, fi)
 
@@ -33,15 +36,23 @@ ans = 0
 with open(fip) as f:
     for idx, line in enumerate(f.readlines() + pad):
         xs = line.strip()
+        print(xs)
+        q = deque(xs[:4])
+        for i in range(4, len(xs)):
+            if len(set(q)) == 4:
+                ans = i
+                break
+            q.append(xs[i])
+            q.popleft()
 
 
 
     print(f'p1 ans: {ans}')
 
 """
-
+How many characters need to be processed before the first start-of-message marker is detected?
 """
-test = 1
+test = 0
 fi = "input.txt" if not test else "ex1.txt"
 fip = join(dir_path, fi)
 
@@ -52,6 +63,14 @@ ans = 0
 with open(fip) as f:
     for idx, line in enumerate(f.readlines() + pad):
         xs = line.strip()
+        print(xs)
+        q = deque(xs[:14])
+        for i in range(14, len(xs)):
+            if len(set(q)) == 14:
+                ans = i
+                break
+            q.append(xs[i])
+            q.popleft()
 
 
 
